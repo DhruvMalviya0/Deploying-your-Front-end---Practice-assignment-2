5b63304b10ab5503153a22536eafe07bea33c2c1
Dhruv Malviya <dhruvnotfoundlol@gmail.com>
2025-02-18 09:59:46 +0530

    pompom

---
+++ home_page/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+          <h1>Dhruv Malviya</h1>
+          <p>Email: your.email@example.com</p>
+          <p>Phone: +1234567890</p>
+      </div>
+  );
+}
+
+export default App;
